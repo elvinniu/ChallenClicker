1c4537c84807e726260a95bebf64479f7d12afac
elvinniu <eniu2@illinois.edu>
2019-04-24 22:16:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/uhhhfinal/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.uhhhfinal;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    public Long geoffCount = 0L;
+    public String geocounter = geoffCount + " geoffs";
+    public String startUpText = "What are you waiting for? Touch him!";
+    private boolean started = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //String startUpText = "What are you waiting for? Touch him!";
+
+        ImageButton button = (ImageButton) findViewById(R.id.geoff);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                started = true;
+                clickGeoff();
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void clickGeoff() {
+        TextView counter = (TextView) findViewById(R.id.counnt);
+        geoffCount++;
+        geocounter = geoffCount + " geoffs";
+        counter.setText(geocounter);
+        TextView startUp = (TextView) findViewById(R.id.startUp);
+        startUp.setText(startUpText);
+        if (started) {
+            startUp.setVisibility(View.GONE);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
