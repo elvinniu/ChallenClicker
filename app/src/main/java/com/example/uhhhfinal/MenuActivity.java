93f784fab22547f40b0b1c0754822b98cb00e852
elvinniu <eniu2@illinois.edu>
2019-04-25 22:49:50 -0500

    jwahchecccc

--- app/src/main/java/com/example/uhhhfinal/MenuActivity.java
+++ app/src/main/java/com/example/uhhhfinal/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.uhhhfinal;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.TimerTask;
 
 public class MenuActivity extends AppCompatActivity {
     public String geoffCounter = MainActivity.GlobalVars.globalChallen + "";
@@ -48,8 +53,44 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(new Intent(MenuActivity.this, MainActivity.class));
             }
         });
+        final boolean isClicked = false;
+        final FloatingActionButton laptop = findViewById(R.id.laptopPage);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isClicked) {
+                    laptop.setBackgroundResource(R.drawable.question_mark);
+                    laptop.setBackgroundColor(Color.WHITE);
+                }
+
+            }
+        });
+
 
     }
+
+    long startTime = 0;
+
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            /*
+            FloatingActionButton laptop = findViewById(R.id.laptopPage);
+            if (MainActivity.GlobalVars.globalChallen >= 10) {
+                laptop.setBackgroundResource(R.drawable.question_mark);
+                laptop.setBackgroundColor(Color.WHITE);
+            }
+*/
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
